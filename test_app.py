5cda71a0191f7ec424d271e6013238b745b306f5
Tanner York <tannerwyork@gmail.com>
2020-07-13 16:21:39 -0500

    Add circleci and unit tests

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import app
+import unittest
+
+
+class TestGifSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.app.testing = True
+
+    def test_index_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_typeahead_route(self):
+        response = self.app.get('/typeahead')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
